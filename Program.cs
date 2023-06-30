e68ba3e4fbdfcea14c8035e53591c558118fe353
Matvey Vizir <112975341+MaTTveIg@users.noreply.github.com>
2023-06-30 15:34:08 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using _7_nuget;
+using Newtonsoft.Json;
+using System.Drawing;
+using TrueColorConsole;
+
+RibbonLED ribbon = new(10);
+void PaintRibbon(RibbonLED ribbon)
+{
+    Random random = new Random();
+    for (int i = 0; i < RibbonLED.size; ++i)
+    {
+        ribbon.parts[i] = '0';
+        for (int j = 0; j < 3; ++j)
+            ribbon.colors[i, j] = random.Next(0, 255);
+    }
+}
+
+void ShowFullRibbon(RibbonLED ribbon)
+{
+    string str = JsonConvert.SerializeObject(ribbon);
+    for (int i = 0; i < RibbonLED.size; ++i)
+    {
+        int r = ribbon.colors[i, 0];
+        int g = ribbon.colors[i, 1];
+        int b = ribbon.colors[i, 2];
+        VTConsole.Write(str, Color.FromArgb(r, g, b));
+    }
+}
+
+void ShowStandartRibbon(RibbonLED ribbon)
+{
+    string str = JsonConvert.SerializeObject(ribbon);
+    for (int i = 0; i < RibbonLED.size; ++i)
+    {
+        int r = ribbon.colors[i, 0];
+        int g = ribbon.colors[i, 1];
+        int b = ribbon.colors[i, 2];
+        VTConsole.Write(Convert.ToString(ribbon.parts[i]), Color.FromArgb(r, g, b));
+    }
+}
+
+PaintRibbon(ribbon);
+ShowStandartRibbon(ribbon);
+
+//ShowStandartRibbon(ribbon);
---
+++ RibbonLED.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrueColorConsole;
+
+namespace _7_nuget
+{
+    internal class RibbonLED
+    {
+        public static int size = 10;
+        public char[] parts = new char[size];
+        public int[,] colors = new int[size, 3];
+        public RibbonLED(int count)
+        {
+           size = count;
+        }
+    }
+}
